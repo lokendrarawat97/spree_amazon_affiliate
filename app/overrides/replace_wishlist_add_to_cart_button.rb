7974ddafde492d53f3d2599f2850340aa3444b8e
Jeff Dutil <jdutil21@gmail.com>
2012-02-14 15:35:51 -0500

    Update Versionfile and add wishlist override.

---
+++ app/overrides/replace_wishlist_add_to_cart_button.rb
@@ -0,0 +1,16 @@
+if Rails.application.railties.all.map(&:railtie_name).include? "spree_wishlist"
+
+  Deface::Override.new(:virtual_path => "spree/wishlists/show",
+                       :name => "replace_wishlist_add_to_cart_button",
+                       :replace => "code[erb-loud]:contains('populate_orders_url')",
+                       :closing_selector => "code[erb-silent]:contains('end')",
+                       :text => %q{<% if product.amazon_id.present? %>
+                                   <%= amazon_affiliate_link(product.amazon_id) %>
+                                 <% else %>
+                                   <%= form_for :order, :url => populate_orders_url do |f| %>
+                                     <%= hidden_field_tag "variants[#{variant.id}]", 1, :size => 3 %>
+                                     <%= link_to t(:add_to_cart), '#', :onclick => "$(this).parent().submit(); return false;" %>
+                                   <% end %>
+                                 <% end %>})
+
+end
